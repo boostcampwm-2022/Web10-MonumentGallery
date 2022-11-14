6fe5e52c590b08be8760bcc72024909e7888f990
lybell <futireflugel@gmail.com>
2022-11-14 16:23:28 +0900

    feat: MongoDB 연결, 연결테스트 DB API 작성

---
+++ server/model/testSchema.js
@@ -0,0 +1,9 @@
+import {model, Schema} from "mongoose";
+// test model
+
+const testSchema = new Schema({
+  username: {type: String, required: true, unique: true},
+  password: {type: String},
+});
+
+export default model("myDBTest", testSchema);

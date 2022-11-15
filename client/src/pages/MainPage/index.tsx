2b84b357739c27934fec05bf76b734815eabe80c
Seyeon Ko <koseyeon97@gmail.com>
2022-11-15 11:36:02 +0900

    feat: notion oauth 인증 버튼에 인증 url 연결

--- client/src/pages/MainPage/index.tsx
+++ client/src/pages/MainPage/index.tsx
@@ -4,6 +4,10 @@ import "./style.scss";
 
 export default function MainPage() {
   const [show, setShow] = useState<boolean>(false);
+  function notionOauthHandler() {
+    window.location.href =
+      "https://api.notion.com/v1/oauth/authorize?client_id=a76e983d-cf3b-4f82-b742-6b3086948345&response_type=code&owner=user&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fnotion%2Fcallback";
+  }
   return (
     <div>
       <button
@@ -16,7 +20,9 @@ export default function MainPage() {
         upload
       </button>
       <FullScreenModal show={show} width="70%" height="55%" setShow={setShow}>
-        <button type="button">notion oauth</button>
+        <button type="button" onClick={notionOauthHandler}>
+          notion oauth
+        </button>
       </FullScreenModal>
     </div>
   );

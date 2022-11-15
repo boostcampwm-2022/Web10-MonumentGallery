faf076cb1f9431e0a191a2c6157ba2e976f7466a
Seyeon Ko <koseyeon97@gmail.com>
2022-11-15 15:11:18 +0900

    feat: create.html 추가

---
+++ client/src/pages/CreatePage/index.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import FullScreenModal from "../../components/modal/FullScreenModal";
+import "./style.scss";
+
+export default function MainPage() {
+  const [show, setShow] = useState<boolean>(false);
+  function getData() {
+    console.log("TEST");
+  }
+  return (
+    <div>
+      <button
+        className="upload-btn"
+        type="button"
+        onClick={() => {
+          setShow(true);
+        }}
+      >
+        upload
+      </button>
+      <FullScreenModal show={show} width="70%" height="55%" setShow={setShow}>
+        <button type="button" onClick={getData}>
+          생성
+        </button>
+      </FullScreenModal>
+    </div>
+  );
+}

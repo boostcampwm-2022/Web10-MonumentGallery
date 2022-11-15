c91fe1d47913090ffbb409ab3e8c249c6ec63fe6
kddkdkk <dlwjddnrbb71@naver.com>
2022-11-15 23:12:52 +0900

    feat: ioredis 사용, redis 연동 확인

--- server/model/accessTokenStore.js
+++ server/model/accessTokenStore.js
@@ -1,30 +1,14 @@
-import redis from "redis";
+import Redis from "ioredis";
 import dotenv from "dotenv";
 dotenv.config();
 
-const redisClient = redis.createClient({
-  url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/0`,
-  legacyMode: true,
+const redisCli = new Redis({
+  port: process.env.REDIS_PORT,
+  host: process.env.REDIS_HOST,
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+  db: 0,
 });
-
-// const redisClient = redis.createClient({
-//   socket: {
-//     host: process.env.REDIS_HOST,
-//     port: process.env.REDIS_PORT,
-//   },
-//   password: process.env.REDIS_PASSWORD,
-// });
-
-redisClient.on("connect", () => {
-  console.log("Redis connected!");
-});
-
-redisClient.on("error", (err) => {
-  console.error("Redis Client Error", err);
-});
-redisClient.connect().then(); // redis v4 연결 (비동기)
-const redisCli = redisClient.v4;
-// const redisCli = redisClient;
 
 export async function saveTokenData(token, data) {
   await redisCli.set(token, JSON.stringify(data));

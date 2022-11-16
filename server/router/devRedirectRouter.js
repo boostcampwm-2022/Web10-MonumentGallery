0dbd3998ac015455f2452a99f0d383084496db27
lybell <futireflugel@gmail.com>
2022-11-16 16:33:47 +0900

    modify: 로그인되지 않은 상태일 때 GET /create를 접근하면 메인 화면으로 리다이렉트

--- server/router/devRedirectRouter.js
+++ server/router/devRedirectRouter.js
@@ -3,6 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/create", (req, res) => {
+  if( req.userid == null ) return res.redirect("/");
   res.redirect("/create.html");
 });
 

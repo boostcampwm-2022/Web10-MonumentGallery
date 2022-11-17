27119f413d71f76d9dc81896f79bb0066cebe177
Lipeya <reljacer@naver.com>
2022-11-17 18:22:48 +0900

    feat: notionService 생성

--- server/router/testRouter.js
+++ server/router/testRouter.js
@@ -2,6 +2,7 @@ import express from "express";
 import { Client } from "@notionhq/client";
 import axios from "axios";
 import TestModel from "../model/testSchema.js";
+import { getContentsFromNotion } from "../service/notionService.js";
 
 const router = express.Router();
 
@@ -33,25 +34,31 @@ router.get("/testGet", async (req, res) => {
 
 router.get("/getData", async (req, res) => {
   const notionAccessToken = req.accessToken;
-  const notion = new Client({ auth: notionAccessToken });
-  const response = await notion.search({
-    filter: { property: "object", value: "page" },
-  });
-  const pageIds = [];
-  response.results.forEach((result) => {
-    if (result.object === "page") {
-      pageIds.push(result.id);
-    }
-  });
-  const pageContents = [];
-  for (let i = 0; i < pageIds.length; i++) {
-    const content = await notion.blocks.children.list({
-      block_id: pageIds[i],
-      page_size: 50,
-    });
-    pageContents.push(content);
-  }
-  res.status(200).json(pageContents);
+  const nowTime = Date.now();
+  // console.log(await getContentsFromNotion(notionAccessToken));
+  // const notion = new Client({ auth: notionAccessToken });
+  // const response = await notion.search({
+  //   filter: { property: "object", value: "page" },
+  // });
+  // const pageIds = [];
+  // console.log(response.results);
+  // response.results.forEach((result) => {
+  //   if (result.object === "page") {
+  //     pageIds.push(result.id);
+  //   }
+  // });
+  // const pageContents = [];
+  // for (let i = 0; i < pageIds.length; i++) {
+  //   const content = await notion.blocks.children.list({
+  //     block_id: pageIds[i],
+  //     page_size: 50,
+  //   });
+  //   getDataFromPageContent(content);
+  //   pageContents.push(content);
+  // }
+
+  res.status(200).json(await getContentsFromNotion(notionAccessToken));
+  console.log(`총 처리 시간: ${Date.now() - nowTime}`);
 });
 
 // FastAPI 연결 확인 test

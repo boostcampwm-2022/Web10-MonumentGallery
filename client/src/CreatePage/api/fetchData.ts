dfe440553bcd58659085d41a8cf1f64fbb4c212c
lybell <futireflugel@gmail.com>
2022-11-17 23:44:06 +0900

    style: typescript 빌드 오류 해결

--- client/src/CreatePage/api/fetchData.ts
+++ client/src/CreatePage/api/fetchData.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
+import { PeriodType, ThemeType } from "../../components/SpaceCreater";
 
-export function fetchData(period, theme) {
+export function fetchData(period: PeriodType | null, theme: ThemeType | null) {
   let data: any[] | null = null;
   const suspender = axios.get("/test/getData", { params: { period, theme } }).then((res) => {
     data = res.data;

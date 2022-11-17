37ea8e2dc0adcdc8d7ee373f5d296b9bd7c5dccc
Seyeon Ko <koseyeon97@gmail.com>
2022-11-17 16:23:43 +0900

    feat: 에러 발생 시 toast 띄우기 예시 구현

--- client/src/MySpacePage/components/UI.tsx
+++ client/src/MySpacePage/components/UI.tsx
@@ -1,22 +1,32 @@
+import axios, { AxiosError } from "axios";
 import { useState } from "react";
 import { toast, Toast } from "../../components/Toast/Toast";
-import { TOAST_LIST } from "../../components/Toast/ToastList";
+import { TOAST_SUCCESS, TOAST_INFO, TOAST_ERROR, TOAST_WARNING } from "../../components/Toast/ToastList";
 import "../style.scss";
 export default function UI() {
-  const [list, setList] = useState<toast[]>([]);
-  function showToast(type: string) {
-    const toast = TOAST_LIST.find((toast) => toast.type === type);
-    if (toast) {
-      setList([...list, toast]);
+  const [toastList, setToastList] = useState<toast[]>([]);
+  async function getResponse() {
+    try {
+      const response = await axios.get("/test/invalid-url");
+      if (response.status === 200) {
+        const toast = TOAST_SUCCESS;
+        setToastList([...toastList, toast]);
+      }
+    } catch (error) {
+      const err = error as AxiosError;
+      if (err.response !== undefined && err.response.status === 404) {
+        const toast = TOAST_ERROR;
+        setToastList([...toastList, toast]);
+      }
     }
   }
   return (
     <>
       <h1 className="logo"> MONUMENT GALLERY (테스트용 UI) </h1>
-      <button className="toast-test-btn" onClick={() => showToast("ERROR")}>
-        토스트 생성 테스트 버튼
+      <button className="toast-test-btn" onClick={() => getResponse()}>
+        토스트 테스트 버튼
       </button>
-      <Toast toastList={list} position="bottom-right" autoDelete={true} autoDeleteTime={2000} />
+      <Toast toastList={toastList} position="bottom-right" autoDelete={true} autoDeleteTime={2000} />
     </>
   );
 }
--- client/src/components/Toast/ToastList.ts
+++ client/src/components/Toast/ToastList.ts
@@ -3,33 +3,31 @@ import errorIcon from "../../assets/images/toast-icon-error.svg";
 import infoIcon from "../../assets/images/toast-icon-info.svg";
 import warningIcon from "../../assets/images/toast-icon-warning.svg";
 
-export const TOAST_LIST = [
-  {
-    id: Math.floor(Math.random() * 101 + 1),
-    type: "SUCCESS",
-    description: "This is a success toast component",
-    backgroundColor: "#5cb85c",
-    icon: checkIcon,
-  },
-  {
-    id: Math.floor(Math.random() * 101 + 1),
-    type: "ERROR",
-    description: "This is an error toast component",
-    backgroundColor: "#d9534f",
-    icon: errorIcon,
-  },
-  {
-    id: Math.floor(Math.random() * 101 + 1),
-    type: "INFO",
-    description: "This is an info toast component",
-    backgroundColor: "#5bc0de",
-    icon: infoIcon,
-  },
-  {
-    id: Math.floor(Math.random() * 101 + 1),
-    type: "WARNING",
-    description: "This is a warning toast component",
-    backgroundColor: "#f0ad4e",
-    icon: warningIcon,
-  },
-];
+export const TOAST_SUCCESS = {
+  id: Math.floor(Math.random() * 101 + 1),
+  type: "SUCCESS",
+  description: "This is a success toast component",
+  backgroundColor: "#5cb85c",
+  icon: checkIcon,
+};
+export const TOAST_ERROR = {
+  id: Math.floor(Math.random() * 101 + 1),
+  type: "ERROR",
+  description: "This is an error toast component",
+  backgroundColor: "#d9534f",
+  icon: errorIcon,
+};
+export const TOAST_INFO = {
+  id: Math.floor(Math.random() * 101 + 1),
+  type: "INFO",
+  description: "This is an info toast component",
+  backgroundColor: "#5bc0de",
+  icon: infoIcon,
+};
+export const TOAST_WARNING = {
+  id: Math.floor(Math.random() * 101 + 1),
+  type: "WARNING",
+  description: "This is a warning toast component",
+  backgroundColor: "#f0ad4e",
+  icon: warningIcon,
+};

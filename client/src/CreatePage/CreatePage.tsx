e4b789316ba281915b1f547a4eda15d8f4647850
kddkdkk <dlwjddnrbb71@naver.com>
2022-11-17 16:03:56 +0900

    feat: React suspense 활용, 로딩 구현

--- client/src/CreatePage/CreatePage.tsx
+++ client/src/CreatePage/CreatePage.tsx
@@ -1,18 +1,14 @@
-import React, { useState } from "react";
+import React, { Suspense, useState } from "react";
 import FullScreenModal from "../components/modal/FullScreenModal";
 import "./style.scss";
-import axios from "axios";
 import { Canvas } from "@react-three/fiber";
 import FloatLayout from "../layouts/FloatLayout";
 import Header from "../components/Header";
+import { fetchData } from "./api/fetchData";
 
 export default function CreatePage() {
   const [show, setShow] = useState<boolean>(true);
-  function getData() {
-    axios.get("/test/getData").then((response) => {
-      console.log(response.data);
-    });
-  }
+  const [fetcher, setFetcher] = useState<{ get: () => void } | boolean>(false);
 
   function showModal() {
     setShow(true);
@@ -34,11 +30,21 @@ export default function CreatePage() {
       <FullScreenModal show={show} width="70%" height="55%" setShow={setShow}>
         <div className="create-modal">
           <span className="make-gallery">갤러리 만들기</span>
-          <button type="button" onClick={getData}>
-            <span>생성하기</span>
-          </button>
+
+          {fetcher ? (
+            <Suspense fallback={<button>로딩중...</button>}>
+              <Data resource={fetcher} />
+            </Suspense>
+          ) : (
+            <button onClick={() => setFetcher(fetchData())}>생성하기</button>
+          )}
         </div>
       </FullScreenModal>
     </main>
   );
+}
+
+function Data({ resource }: { resource: any }) {
+  const data = resource.get();
+  return <div style={{ overflow: "scroll" }}>{JSON.stringify(data, null, 2)}</div>;
 }

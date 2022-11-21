62b7afd4a454a361112721f77e7c0447251dc948
Seyeon Ko <koseyeon97@gmail.com>
2022-11-21 16:51:08 +0900

    feat:konlpy nouns 메소드를 이용해 키워드 추출

--- python/README.md
+++ python/README.md
@@ -22,3 +22,9 @@
 ### FastAPI 서버 실행
 
 `uvicorn main:app --reload`
+
+# konlpy 환경 주의사항
+
+m1 mac의 경우 JDK 버전 문제로 인해 실행이 안될 수 있습니다.
+jdk17 버전을 설치하시면 정상 작동하는 것을 확인하였습니다.(아래 링크 참고)
+https://github.com/hexists/konlpy/wiki/konlpy-X-m1-silicon
--- python/main.py
+++ python/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from typing import Dict,List,Optional
 from pydantic import BaseModel
-# from konlpy.tag import Kkma
-# from konlpy.utils import pprint
+from konlpy.tag import Kkma
+from konlpy.utils import pprint
+from collections import defaultdict
+
+kkma = Kkma()
 
 app = FastAPI()
 
@@ -14,7 +17,50 @@ class Page(BaseModel):
     paragraph: Optional[List[str]]
 class NotionData(BaseModel):
     pages: Dict[str,Page]
+class preprocessedPage(BaseModel):
+    keywords: Dict[str,int]
+    title: str
+    h1: Optional[List[str]]
+    h2: Optional[List[str]]
+    h3: Optional[List[str]]
+    paragraph: Optional[List[str]]
+class preprocessedNotionData(BaseModel):
+    totalKeywords: Dict[str,int]
+    pages: Dict[str,preprocessedPage]
+
+
+class preprocessedPage:
+    def __init__(self):
+        self.keywords = defaultdict(int)
+        self.title = []
+        self.h1_keywords = []
+        self.h2_keywords = []
+        self.h3_keywords = []
+        self.p_keywords = []
+class preprocessedNotionData:
+     def __init__(self):
+        self.totalKeywords = defaultdict(int)
+        self.ppPages = {}
 
 @app.post("/preprocess")
 def root(notionData:NotionData):
-    return notionData
+    ppData = preprocessedNotionData()
+    for page_id,page in notionData.pages.items():
+        ppData.ppPages[page_id] = preprocessedPage()
+        for sentense in page.h1:
+            for keyword in kkma.nouns(sentense):
+                ppData.totalKeywords[keyword]+=1
+                ppData.ppPages[page_id].h1_keywords.append(keyword)
+        for sentense in page.h2:
+            for keyword in kkma.nouns(sentense):
+                ppData.totalKeywords[keyword]+=1
+                ppData.ppPages[page_id].h2_keywords.append(keyword)
+        for sentense in page.h3:
+            for keyword in kkma.nouns(sentense):
+                ppData.totalKeywords[keyword]+=1
+                ppData.ppPages[page_id].h3_keywords.append(keyword)
+        for sentense in page.paragraph:
+            for keyword in kkma.nouns(sentense):
+                ppData.totalKeywords[keyword]+=1
+                ppData.ppPages[page_id].p_keywords.append(keyword)
+    return ppData

073e59b63e76855cb2d9edf969593cdd15f4b5f3
Seyeon Ko <koseyeon97@gmail.com>
2022-11-21 14:27:07 +0900

    feat:전송받은 mockdata의 데이터 타입 관리

--- python/main.py
+++ python/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from typing import Dict,List,Optional
+from pydantic import BaseModel
 # from konlpy.tag import Kkma
 # from konlpy.utils import pprint
 
 app = FastAPI()
 
-@app.get("/preprocess")
-def root():
-    return {"message": "Hello World"}
+class Page(BaseModel):
+    title: str
+    h1: Optional[List[str]]
+    h2: Optional[List[str]]
+    h3: Optional[List[str]]
+    paragraph: Optional[List[str]]
+class NotionData(BaseModel):
+    pages: Dict[str,Page]
+
+@app.post("/preprocess")
+def root(notionData:NotionData):
+    return notionData

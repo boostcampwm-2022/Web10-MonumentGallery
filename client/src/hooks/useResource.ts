8588889e00f5c072ed2ede2143ae889a10538936
kddkdkk <dlwjddnrbb71@naver.com>
2022-11-21 21:15:38 +0900

    refactor: useResource generic 추가

--- client/src/hooks/useResource.ts
+++ client/src/hooks/useResource.ts
@@ -2,10 +2,10 @@ import { AxiosRequestConfig } from "axios";
 import { useMemo } from "react";
 import { Resource } from "../utils/suspender";
 
-export default function useResource(
-  resource: Resource,
+export default function useResource<T>(
+  resource: Resource<T>,
   options: AxiosRequestConfig,
-  callback: (res: unknown) => void = () => null,
+  callback: (res: T) => void = () => null,
 ) {
   const data = useMemo(() => {
     const res = resource.read(options);

1e32d6031f0c721022b4935288a5e2339464f648
kddkdkk <dlwjddnrbb71@naver.com>
2022-11-22 18:04:25 +0900

    feat: 페이지 영역 진입 시 타이틀 애니메이션트리거

--- client/src/GalleryPage/Gallery.tsx
+++ client/src/GalleryPage/Gallery.tsx
@@ -6,11 +6,10 @@ import Light from "./mapObjects/Light";
 import MovementController from "./components/MovementController";
 import ViewRotateController from "./components/ViewRotateController";
 import dummyData from "./dummyData";
-import galleryStore from "../store/gallery.store";
 
+// const camera = new THREE.PerspectiveCamera(75, 1920 / 1080, 0.1, 100);
+// camera.position.set(0, 1.5, 2);
 export default function Gallery() {
-  // const { data } = galleryStore();
-
   return (
     <Canvas className="canvas-inner" camera={{ fov: 75, near: 0.1, far: 100, position: [0, 1.5, 2] }}>
       <Light />
--- client/src/GalleryPage/mapObjects/GalleryPageIsland.tsx
+++ client/src/GalleryPage/mapObjects/GalleryPageIsland.tsx
@@ -1,10 +1,67 @@
 import { IGalleryPageData } from "../../@types/gallery";
 import Island from "./Island";
+import React, { useRef, useState } from "react";
+import { a, useSpring } from "@react-spring/three";
+import { Text } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
+import MapoFlowerIsland from "../../assets/fonts/MapoFlowerIsland.otf";
 
-export default function GalleryPageIsland({ position }: IGalleryPageData) {
+interface AnimatedTitleProps {
+  position: [x: number, y: number, z: number];
+  text: string;
+}
+
+function AnimatedTitle({ position, text }: AnimatedTitleProps) {
+  const [active, setActive] = useState(0);
+  const [action, setAction] = useState(false);
+  const { camera } = useThree();
+  const meshRef = useRef<THREE.Group>(null);
+
+  const { spring } = useSpring({
+    spring: active,
+    config: { tension: 500, friction: 150, precision: 0.04 },
+    onStart: () => setAction(true),
+    onRest: () => setAction(false),
+  });
+
+  const scale = spring.to([0, 1], [0, 4]);
+  const rotation = spring.to([0, 1], [0, Math.PI * 4]);
+
+  useFrame(() => {
+    if (!action) meshRef.current?.lookAt(camera.position);
+    const { x, z: y } = camera.position;
+    if (x > position[0] - 8 && x < position[0] + 8 && y > position[2] - 8 && y < position[2] + 8) {
+      if (!active) setActive(+!active);
+    } else {
+      if (active) setActive(+!active);
+    }
+  });
+
+  return (
+    <a.group ref={meshRef} position={position} position-y={scale}>
+      <a.mesh rotation-y={rotation} scale-z={scale} onClick={() => setActive(+!active)}>
+        <Text
+          visible={!(!active && !action)}
+          font={MapoFlowerIsland}
+          color="black"
+          fontSize={0.4}
+          anchorX="center"
+          anchorY="middle"
+        >
+          {text}
+        </Text>
+      </a.mesh>
+    </a.group>
+  );
+}
+
+export default function GalleryPageIsland({ position, title }: IGalleryPageData) {
+  const [x, z] = position;
+
   return (
     <>
-      <Island x={position[0]} z={position[1]} />
+      <AnimatedTitle position={[x, 0, z]} text={title} />
+      <Island x={x} z={z} />
     </>
   );
 }

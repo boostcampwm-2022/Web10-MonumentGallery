3c9cc7f9763290d13e1d8788e048dc376cf321be
kddkdkk <dlwjddnrbb71@naver.com>
2022-11-24 12:09:07 +0900

    refactor: SkyCloud 애니메이션 useMemo 추가

--- client/src/GalleryPage/mapObjects/SkyCloud.tsx
+++ client/src/GalleryPage/mapObjects/SkyCloud.tsx
@@ -1,5 +1,6 @@
-import { animated } from "@react-spring/three";
+import { animated, Interpolation } from "@react-spring/three";
 import { Cloud } from "@react-three/drei";
+import { useMemo } from "react";
 import { ITriggeredSpringState } from "../../@types/common";
 
 interface SkyCloudProps {
@@ -8,8 +9,8 @@ interface SkyCloudProps {
 
 export default function SkyCloud({ animator }: SkyCloudProps) {
   const { spring } = animator;
+  const positionY: Interpolation<number, number> = useMemo(() => spring.to([0, 1], [-5, -10]), []);
 
-  const positionY = spring.to([0, 1], [-5, -10]);
   return (
     <animated.group position-y={positionY}>
       <Cloud position={[0, 0, 0]} speed={0.2} opacity={0.5} />

fa685161c28a431eedc62b831433298743d72e04
Lipeya <reljacer@naver.com>
2022-11-24 11:21:39 +0900

    feat: Gallery Schema 생성 및 CR 확인

---
+++ server/model/gallerySchema.js
@@ -0,0 +1,32 @@
+import { model, Schema } from "mongoose";
+
+const gallerySchema = new Schema({
+  theme: { type: String, required: true },
+  totalKeywords: {
+    type: Map,
+    of: Number,
+    required: true,
+  },
+  pages: [
+    {
+      position: [Number],
+      keywords: { type: Map, of: Number, default: "-" },
+      title: { type: String, required: true },
+      subTitle: [
+        {
+          hType: String,
+          text: String,
+        },
+      ],
+      links: [
+        {
+          href: String,
+          favicon: String,
+        },
+      ],
+    },
+  ],
+  nodes: [[Number]],
+});
+
+export default model("myDBGalleryTest", gallerySchema);

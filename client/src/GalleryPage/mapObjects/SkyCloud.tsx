6dc7f1039546719c680d4e2e02d60f52862d6759
kddkdkk <dlwjddnrbb71@naver.com>
2022-11-24 12:06:50 +0900

    feat: SkyCloud 추가 및 애니메이션 트리거에 포함

---
+++ client/src/GalleryPage/mapObjects/SkyCloud.tsx
@@ -0,0 +1,22 @@
+import { animated } from "@react-spring/three";
+import { Cloud } from "@react-three/drei";
+import { ITriggeredSpringState } from "../../@types/common";
+
+interface SkyCloudProps {
+  animator: ITriggeredSpringState;
+}
+
+export default function SkyCloud({ animator }: SkyCloudProps) {
+  const { spring } = animator;
+
+  const positionY = spring.to([0, 1], [-5, -10]);
+  return (
+    <animated.group position-y={positionY}>
+      <Cloud position={[0, 0, 0]} speed={0.2} opacity={0.5} />
+      <Cloud position={[0, 0, 7]} speed={0.2} opacity={0.5} />
+      <Cloud position={[0, 0, -7]} speed={0.2} opacity={0.5} />
+      <Cloud position={[7, 0, 0]} speed={0.2} opacity={0.5} />
+      <Cloud position={[-7, 0, 0]} speed={0.2} opacity={0.5} />
+    </animated.group>
+  );
+}

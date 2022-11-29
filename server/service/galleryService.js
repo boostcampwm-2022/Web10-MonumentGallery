be45b8de6d4655be7689a26cd90dfae1f0174e5e
Lipeya <reljacer@naver.com>
2022-11-29 17:57:05 +0900

    feat: galleryService 생성

---
+++ server/service/galleryService.js
@@ -0,0 +1,28 @@
+import { saveGallery } from "./dataSaveService.js";
+import { processDataFromRawContent, processDataForClient } from "./dataProcessService.js";
+import { getImagePixelsFromPages } from "./imageProcessService.js";
+import { getConnectionSSE, endConnectionSSE, writeMessageSSE } from "./sseService.js";
+
+export async function createGallery(req, res) {
+  req.connection.setTimeout(60 * 5 * 1000); //5분
+  getConnectionSSE(res);
+  const notionAccessToken = req.accessToken;
+  const { period = "all", theme = "dream" } = req.query;
+  writeMessageSSE(JSON.stringify({ kind: "노션 데이터 불러오는 중...", progress: 25, data: {} }), res);
+  const notionRawContent = await getRawContentsFromNotion(notionAccessToken, period);
+  writeMessageSSE(JSON.stringify({ kind: "노션 데이터 불러오기 완료", progress: 50, data: {} }), res);
+
+  writeMessageSSE(JSON.stringify({ kind: "이미지 가공 중...", progress: 55, data: {} }), res);
+  const notionImageContent = await getImagePixelsFromPages(notionRawContent);
+  writeMessageSSE(JSON.stringify({ kind: "이미지 가공 완료", progress: 60, data: {} }), res);
+
+  writeMessageSSE(JSON.stringify({ kind: "키워드 추출 중...", progress: 65, data: {} }), res);
+  const processedNotionContent = await processDataFromRawContent(notionImageContent, theme);
+  writeMessageSSE(JSON.stringify({ kind: "키워드 추출 완료", progress: 70, data: {} }), res);
+
+  writeMessageSSE(JSON.stringify({ kind: "DB에서 데이터 저장 중...", progress: 80, data: {} }), res);
+  const galleryID = await saveGallery(userID, processedNotionContent);
+  writeMessageSSE(JSON.stringify({ kind: "DB에서 데이터 저장 완료", progress: 85, data: {} }), res);
+
+  return galleryID;
+}

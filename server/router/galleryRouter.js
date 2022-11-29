180d58ac155703e6977eca8850c61705fa90a1cd
Lipeya <reljacer@naver.com>
2022-11-29 17:57:23 +0900

    refactor: 반복되는 로직 모듈화

--- server/router/galleryRouter.js
+++ server/router/galleryRouter.js
@@ -1,17 +1,11 @@
 import express from "express";
 import { authMiddleware, catchAuthError } from "../middleware/authMiddleware.js";
-import { getRawContentsFromNotion } from "../service/getNotionContentService.js";
-import { processDataFromRawContent, processDataForClient } from "../service/dataProcessService.js";
-import {
-  saveGallery,
-  loadGallery,
-  loadLastGallery,
-  getGalleryHistory,
-  getLastGalleryID,
-} from "../service/dataSaveService.js";
+import { processDataForClient } from "../service/dataProcessService.js";
+import { loadGallery, loadLastGallery, getGalleryHistory, getLastGalleryID } from "../service/dataSaveService.js";
 import { asyncHandler } from "../utils/utils.js";
 import { updateShareState } from "../model/galleryModel.js";
-import { getImagePixelsFromPages } from "../service/imageProcessService.js";
+import { createGallery } from "../service/galleryService.js";
+import { writeMessageSSE, endConnectionSSE } from "../service/sseService.js";
 
 const router = express.Router();
 
@@ -20,23 +14,32 @@ router.post(
   authMiddleware,
   catchAuthError,
   asyncHandler(async (req, res) => {
-    //timeout 5분..
-    req.connection.setTimeout(60 * 5 * 1000);
     //duration= 2w||1m||3m||1y
     console.log("page making start");
     const userID = req.userid;
-    const notionAccessToken = req.accessToken;
     const nowTime = Date.now();
-    const { period = "all", theme = "dream" } = req.query;
 
-    const notionRawContent = await getRawContentsFromNotion(notionAccessToken, period);
-    const notionImageContent = await getImagePixelsFromPages(notionRawContent);
-    // console.log(notionImageContent);
-    const processedNotionContent = await processDataFromRawContent(notionImageContent, theme);
-    const galleryID = await saveGallery(userID, processedNotionContent);
+    const galleryID = await createGallery(req, res);
 
     console.log(`총 처리 시간: ${Date.now() - nowTime}`);
-    res.status(200).json({ page: `/gallery/${userID}/${galleryID}` });
+    endConnectionSSE(res, { page: `/gallery/${userID}/${galleryID}` });
+  }),
+);
+
+router.post(
+  "/gallery/sync",
+  authMiddleware,
+  asyncHandler(async (req, res) => {
+    const userID = req.userid;
+    const nowTime = Date.now();
+    const galleryID = await createGallery(req, res);
+
+    writeMessageSSE(JSON.stringify({ kind: "DB에서 데이터 불러오는 중...", progress: 90, data: data }), res);
+    const result = await loadGallery(userID, galleryID);
+    writeMessageSSE(JSON.stringify({ kind: "DB에서 데이터 불러오기 완료", progress: 95, data: data }), res);
+
+    console.log(`총 처리 시간: ${Date.now() - nowTime}`);
+    endConnectionSSE(res, processDataForClient(result));
   }),
 );
 
@@ -60,21 +63,6 @@ router.get(
 
     const result = await loadGallery(targetUserID, galleryID);
     res.status(200).json({ gallery: processDataForClient(result), userId: targetUserID });
-  }),
-);
-
-router.post(
-  "/gallery/sync",
-  authMiddleware,
-  asyncHandler(async (req, res) => {
-    const userID = req.userid;
-    const notionAccessToken = req.accessToken;
-    const { period = "all", theme = "dream" } = req.query;
-    const notionRawContent = await getRawContentsFromNotion(notionAccessToken, period);
-    const processedNotionContent = await processDataFromRawContent(notionRawContent, theme);
-    const galleryID = await saveGallery(userID, processedNotionContent);
-    const result = await loadGallery(userID, galleryID);
-    res.status(200).json(processDataForClient(result));
   }),
 );
 

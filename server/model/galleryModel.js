0bcb001f3f53aee2b7a514ce7b82be37363df095
Lipeya <reljacer@naver.com>
2022-12-01 01:01:32 +0900

    refactor: 서버 계층화(Transaction 서비스로 이동 등)

--- server/model/galleryModel.js
+++ server/model/galleryModel.js
@@ -66,55 +66,44 @@ import Gallery from "../schema/gallerySchema.js";
 //     return null;
 //   }
 // }
+export async function saveGallery(galleryData, session) {
+  const [createdData] = await Gallery.create([galleryData], { session });
+  return createdData._id.valueOf();
 }
 
+export async function findGalleryByID(galleryID) {
+  if (galleryID === null) return null;
+  return await Gallery.findById(galleryID);
 }
-
-async function saveGallery(userID, galleryData) {
-  const session = await startSession();
-  try {
-    session.startTransaction();
-    const [createdData] = await Gallery.create([galleryData], { session });
-    const galleryID = createdData._id.valueOf();
-    await updateUserGallery(userID, galleryID, session);
-    await session.commitTransaction();
-    session.endSession();
-    return galleryID;
-  } catch (err) {
-    await session.abortTransaction();
-    session.endSession();
-    console.log(err);
-    return null;
-  }
+export async function updateGalleryView(galleryID, viewers, views, session) {
+  await Gallery.updateOne({ _id: galleryID }, { views: views + 1, viewers }).session(session);
 }
 
 // export async function loadGallery(requestUserData, userID, galleryID) {
 //   const { ipaddr, requestUserID } = requestUserData;
 
-  const user = await User.findOne({ userID });
-  const { history } = user;
-  if (!history.has(galleryID)) return { success: false, err: "don't have gallery" };
+//   if (typeof galleryID !== "string" || galleryID.length !== 24) {
+//     return { success: false, err: "bad_request" };
+//   }
+//   if ((await User.exists({ userID })) === false) return { success: false, err: "no user" };
 
-  const galleryData = await Gallery.findById(galleryID);
-  if (galleryData === null) return { success: false, err: "no gallery" };
+//   const user = await User.findOne({ userID });
+//   const { history } = user;
+//   if (!history.has(galleryID)) return { success: false, err: "don't have gallery" };
 
-  console.log({ id: userID, requestUserID });
+//   const galleryData = await Gallery.findById(galleryID);
+//   if (galleryData === null) return { success: false, err: "no gallery" };
 
-  if (!user.isShared && userID !== requestUserID) return { success: false, err: "not authorized" };
+//   console.log({ id: userID, requestUserID });
 
-  // 조회수 관련 로직
-  const { views, viewers } = galleryData;
+//   if (!user.isShared && userID !== requestUserID) return { success: false, err: "not authorized" };
 
-  const now = new Date().toLocaleDateString();
-  const iphash = hash(ipaddr);
-  const viewed = viewers.get(iphash) === now;
+//   // 조회수 관련 로직
+//   const { views, viewers } = galleryData;
 
-  if (iphash && (!viewed || ipaddr === "development")) {
-    viewers.set(iphash, now);
-    await Gallery.updateOne({ _id: galleryData._id }, { views: views + 1, viewers });
-  }
-  return { success: true, data: galleryData };
-}
+//   const now = new Date().toLocaleDateString();
+//   const iphash = hash(ipaddr);
+//   const viewed = viewers.get(iphash) === now;
 
 //   if (iphash && (!viewed || ipaddr === "development")) {
 //     viewers.set(iphash, now);

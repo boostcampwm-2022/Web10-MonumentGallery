9581a8accf173ab936d2f75adce09419a46195b8
Seyeon Ko <koseyeon97@gmail.com>
2022-12-01 15:52:40 +0900

    feat: 캐릭터 회전 애니메이션 구현

--- client/src/MainPage/mapObjects/Ghost.tsx
+++ client/src/MainPage/mapObjects/Ghost.tsx
@@ -1,11 +1,15 @@
 import * as THREE from "three";
-import React, { useState, useRef } from "react";
+import React, { useMemo, useState, useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 import { GLTF } from "three-stdlib";
 import GhostGlb from "../../assets/models/monument_ghost.glb?url";
 import { useFrame, useThree } from "@react-three/fiber";
 import TPVCollisionPlayerBody from "../components/TPVCollisionPlayerBody";
 import { Vector3Array } from "@react-three/rapier";
+import { useSpring, animated, Interpolation } from "@react-spring/three";
+import useTriggeredSpring from "../../hooks/useTriggeredSpring";
+import GHOST_ROTATE_ANIMATION from "../animations/ghost.animation";
+import { MathUtils } from "three";
 
 type GLTFResult = GLTF & {
   nodes: {
@@ -28,23 +32,38 @@ type GLTFResult = GLTF & {
 // type GLTFActions = Record<ActionName, THREE.AnimationAction>;
 
 const DIRECTION = {
-  IDLE: "IDLE",
-  E: "East",
-  SE: "SouthEast",
-  S: "South",
-  SW: "SouthWest",
-  W: "West",
-  NW: "NorthWest",
-  N: "North",
-  NE: "NorthEast",
+  E: ["E", (Math.PI / 4) * 3],
+  SE: ["SE", Math.PI / 2],
+  S: ["S", Math.PI / 4],
+  SW: ["SW", 0],
+  W: ["W", -Math.PI / 4],
+  NW: ["NW", -Math.PI / 2],
+  N: ["N", (-Math.PI / 4) * 3],
+  NE: ["NE", Math.PI],
 } as const;
+
+type DIRECTION = typeof DIRECTION[keyof typeof DIRECTION];
 
 function Ghost({ ghostParent }: { ghostParent: THREE.Group | null }) {
   const { nodes, materials } = useGLTF(GhostGlb) as unknown as GLTFResult;
   const { camera } = useThree();
+  const collisionRef = useRef<THREE.Group>(null);
   const ghostRef = useRef<THREE.Group>(null);
-  const [rotate, setRotate] = useState();
-  const [position, setPosition] = useState<Vector3Array>([0, 0, 0]);
+  const [animationTrigged, setAnimationTrigged] = useState(false);
+  const { spring, ready, playing } = useTriggeredSpring(animationTrigged, {
+    tension: 1000,
+    friction: 150,
+    precision: 0.2,
+  });
+
+  const [curRotation, setCurRotation] = useState<DIRECTION>(DIRECTION.S);
+  const [destRotation, setDestRotation] = useState<DIRECTION>(DIRECTION.S);
+  const [distRotation, setDistRotation] = useState(0);
+  // 변화값
+  const rotation = useMemo<Interpolation<number, number>>(
+    () => spring.to([0, 1], [curRotation[1], curRotation[1] + distRotation]),
+    [curRotation, distRotation],
+  );
 
   function getDirection(directionVector: [x: number, z: number]) {
     const [x, z] = directionVector;
@@ -56,7 +75,7 @@ function Ghost({ ghostParent }: { ghostParent: THREE.Group | null }) {
     if (x === -1 && z === 0) return DIRECTION.NW;
     if (x === -1 && z === -1) return DIRECTION.N;
     if (x === 0 && z === -1) return DIRECTION.NE;
-    return DIRECTION.IDLE;
+    return false;
   }
 
   function calDirection(prev: Vector3Array, next: Vector3Array): [x: number, z: number] {
@@ -71,44 +90,44 @@ function Ghost({ ghostParent }: { ghostParent: THREE.Group | null }) {
   useFrame(({ clock }) => {
     if (!ghostParent) return;
     if (!ghostRef.current) return;
-    camera.lookAt(ghostRef.current.position);
     const { x, y, z } = camera.position;
-    const prevPosition = position;
+    const prevPosition = ghostRef.current.position.toArray();
     const newPosition: Vector3Array = [x - 10, y - 13, z - 10];
     const directionVector = calDirection(prevPosition, newPosition);
     const direction = getDirection(directionVector);
-    setPosition(newPosition);
-    // TODO: react-spring으로 바뀐 방향에 따라 애니메이션 넣어주기, 상태값으로 관리
-    switch (direction) {
-      case DIRECTION.IDLE:
-        ghostRef.current.position.y += Math.sin(clock.getElapsedTime() * 5) * 0.004;
-        return;
-      case DIRECTION.E:
-        ghostRef.current.rotateY(Math.PI);
-        return;
-      case DIRECTION.SE:
-        return;
-      case DIRECTION.S:
-        return;
-      case DIRECTION.SW:
-        return;
-      case DIRECTION.W:
-        return;
-      case DIRECTION.NW:
-        return;
-      case DIRECTION.N:
-        return;
-      case DIRECTION.NE:
-        return;
-      default:
-        return;
+    collisionRef.current?.position.set(newPosition[0], newPosition[1], newPosition[2]);
+    ghostRef.current.position.set(newPosition[0], newPosition[1], newPosition[2]);
+    // TODO: react-spring으로 바뀐 방향에 따라 애니메이션 넣어주기
+    camera.lookAt(new THREE.Vector3(ghostRef.current.position.x, 0, ghostRef.current.position.z));
+    /*
+      1. 애니메이션 트리거 해야됨
+      2. 애니메이션이 재생중이면 트리거하면 안됨
+    */
+
+    console.log({ direction, ready, playing });
+
+    if (!direction) {
+      setAnimationTrigged(false);
+      ghostRef.current.position.y += Math.sin(clock.getElapsedTime() * 5) * 0.004;
+      setCurRotation(destRotation);
+      return;
     }
+
+    if (curRotation === direction) return;
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    setDistRotation(GHOST_ROTATE_ANIMATION[curRotation[0]][direction[0]]);
+    setDestRotation(direction);
+    setAnimationTrigged(true);
   });
 
   return (
     <>
-      <TPVCollisionPlayerBody position={position} />
-      <group position={position} ref={ghostRef} scale={[0.5, 0.5, 0.5]} dispose={null}>
+      <group ref={collisionRef}>
+        <TPVCollisionPlayerBody position={[0, 0, 0]} />
+      </group>
+      <animated.group rotation-y={rotation} position={[0, 0, 0]} ref={ghostRef} scale={[0.5, 0.5, 0.5]} dispose={null}>
         <group name="Scene">
           <mesh
             name="mouth"
@@ -148,7 +167,7 @@ function Ghost({ ghostParent }: { ghostParent: THREE.Group | null }) {
             scale={0.16}
           />
         </group>
-      </group>
+      </animated.group>
     </>
   );
 }

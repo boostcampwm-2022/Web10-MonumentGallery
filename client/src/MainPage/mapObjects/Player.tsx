dfa7d79f56f978f66800356bbcb6a11731c08d29
lybell <futireflugel@gmail.com>
2022-12-05 16:58:57 +0900

    feat: 이동 방향에 맞는 플레이어 회전 구현

--- client/src/MainPage/mapObjects/Player.tsx
+++ client/src/MainPage/mapObjects/Player.tsx
@@ -1,12 +1,24 @@
-import { useRef, useMemo } from "react";
-import { Vector3 } from "three";
+import { useState, useEffect, useRef, useMemo } from "react";
+import { Vector3, Quaternion, Euler } from "three";
 import { useFrame } from "@react-three/fiber";
 
 import Ghost from "./Ghost";
 import { useKeyMovement } from "../../hooks/useKeyMovement";
 import type { Group } from "three";
 
-const _vector3 = new Vector3();
+const _euler = new Euler(0, 0, 0, "YXZ");
+
+function getNewRotation(newDir: Vector3, currentRot: Quaternion) {
+  const { x, z } = newDir;
+  if (x === 0 && z === 0) {
+    _euler.setFromQuaternion(currentRot);
+    _euler.x = 0;
+    _euler.z = 0;
+    return new Quaternion().setFromEuler(_euler);
+  }
+  _euler.set(Math.PI / 8, Math.atan2(x, z), 0);
+  return new Quaternion().setFromEuler(_euler);
+}
 
 function Player() {
   const isPressed = useKeyMovement();
@@ -19,9 +31,20 @@ function Player() {
     if (isPressed("Left")) right--;
     return new Vector3(right, 0, front).normalize();
   }, [isPressed("Front"), isPressed("Back"), isPressed("Left"), isPressed("Right")]);
+  const [prevRotation, setPrevRotation] = useState(new Quaternion());
+  const [currentRotation, setCurrentRotation] = useState(new Quaternion());
 
   const ghostRef = useRef<Group>(null);
   const speed = 10;
+
+  useEffect(() => {
+    if (!ghostRef.current) return;
+    const ghostRotation = ghostRef.current.quaternion;
+
+    const newRotation = getNewRotation(moveDirection, currentRotation);
+    setPrevRotation(ghostRotation.clone());
+    setCurrentRotation(newRotation);
+  }, [moveDirection]);
 
   useFrame(({ camera }, delta) => {
     if (!ghostRef.current) return;
@@ -35,7 +58,7 @@ function Player() {
   });
 
   return (
-    <group position-y={2} ref={ghostRef}>
+    <group position-y={2} quaternion={currentRotation} ref={ghostRef}>
       <Ghost />
     </group>
   );

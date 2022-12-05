fb301ab234225d1e5f9e41b31097d9c2ed631ab9
lybell <futireflugel@gmail.com>
2022-12-05 16:33:09 +0900

    refactor: 플레이어의 이동 벡터를 state의 형식으로 변경

--- client/src/MainPage/mapObjects/Player.tsx
+++ client/src/MainPage/mapObjects/Player.tsx
@@ -1,37 +1,33 @@
-import { useRef } from "react";
+import { useRef, useMemo } from "react";
 import { Vector3 } from "three";
 import { useFrame } from "@react-three/fiber";
 
 import Ghost from "./Ghost";
 import { useKeyMovement } from "../../hooks/useKeyMovement";
 import type { Group } from "three";
-import { Box } from "@react-three/drei";
 
 const _vector3 = new Vector3();
 
 function Player() {
   const isPressed = useKeyMovement();
-  const ghostRef = useRef<Group>(null);
-  const speed = 10;
-
-  function getMoveVector() {
+  const moveDirection = useMemo(() => {
     let front = 0;
     let right = 0;
     if (isPressed("Front")) front--;
     if (isPressed("Back")) front++;
     if (isPressed("Right")) right++;
     if (isPressed("Left")) right--;
-    _vector3.set(right, 0, front);
-    _vector3.normalize();
-    return _vector3;
-  }
+    return new Vector3(right, 0, front).normalize();
+  }, [isPressed("Front"), isPressed("Back"), isPressed("Left"), isPressed("Right")]);
+
+  const ghostRef = useRef<Group>(null);
+  const speed = 10;
 
   useFrame(({ camera }, delta) => {
     if (!ghostRef.current) return;
     const ghostPos = ghostRef.current.position;
 
-    const moveVector = getMoveVector(delta);
-    ghostPos.addScaledVector(moveVector, speed * delta);
+    ghostPos.addScaledVector(moveDirection, speed * delta);
     camera.position.copy(ghostPos);
     camera.position.x += 10;
     camera.position.y += 15;

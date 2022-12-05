6f809ccc5fd726d3b52f496e7dfd6fa40c16635e
Lipeya <reljacer@naver.com>
2022-12-05 21:38:49 +0900

    feat: seq 제거

--- server/schema/userSchema.js
+++ server/schema/userSchema.js
@@ -7,7 +7,6 @@ const userSchema = new Schema({
   userID: { type: String, required: true, unique: true },
   isShared: { type: Boolean, default: false },
   randIdx: { type: Number, default: 0 },
-  seq: { type: Number, default: 0 },
   lastShareModified: { type: Date, default: new Date() },
   lastModified: { type: Date, default: new Date() },
   history: {

05921a89f1bf4d6c6f511843de93ba017e828b41
kddkdkk <dlwjddnrbb71@naver.com>
2022-12-06 23:22:00 +0900

    fix: ViewsCount가 분리 후 사용 안되었던 것을 추가함

--- client/src/GalleryPage/components/ShareButton/index.tsx
+++ client/src/GalleryPage/components/ShareButton/index.tsx
@@ -8,6 +8,7 @@ import userStore from "../../../store/user.store";
 
 import SharedIcon from "../../../assets/images/shared.svg";
 import ProtectedIcon from "../../../assets/images/protected.svg";
+import ViewsCount from "../ViewsCount";
 
 export default function ShareButton() {
   const [showShareModal, setShowShareModal] = useState(false);
@@ -21,11 +22,13 @@ export default function ShareButton() {
   return (
     <>
       <HoverButton
-        className="sync"
+        className="share"
         img={isShared ? SharedIcon : ProtectedIcon}
         caption={isShared ? "전체에게 공개됨" : "전체에게 비공개됨"}
         onClick={onClick}
-      />
+      >
+        <ViewsCount />
+      </HoverButton>
       <FullScreenModal css={{ width: "400px", height: "230px" }} show={showShareModal} setShow={setShowShareModal}>
         <ShareModal onShareButtonClick={() => setShowShareModal(false)} />
       </FullScreenModal>
--- client/src/components/buttons/HoverButton.tsx
+++ client/src/components/buttons/HoverButton.tsx
@@ -1,18 +1,23 @@
 import "./HoverButton.scss";
 import type { MouseEvent } from "react";
+import React from "react";
 
 interface HoverButtonProps {
   onClick?: (e: MouseEvent<HTMLButtonElement>) => void;
   img: string;
   caption: string;
   className?: string;
+  children?: React.ReactNode;
 }
 
-export default function HoverButton({ onClick, img, caption, className }: HoverButtonProps) {
+export default function HoverButton({ onClick, img, caption, className, children }: HoverButtonProps) {
   return (
-    <button className={`hover-button${className ? " " + className : ""}`} onClick={onClick ?? undefined}>
-      <img width={30} height={30} src={img} alt={caption}></img>
-      <div className="hover-caption">{caption}</div>
-    </button>
+    <div>
+      <button className={`hover-button${className ? " " + className : ""}`} onClick={onClick ?? undefined}>
+        <img width={30} height={30} src={img} alt={caption}></img>
+        <div className="hover-caption">{caption}</div>
+      </button>
+      {children}
+    </div>
   );
 }

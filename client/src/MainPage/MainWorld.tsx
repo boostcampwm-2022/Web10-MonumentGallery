63fd1870ab838d471c168c0b38c090b5f1203e9e
kddkdkk <dlwjddnrbb71@naver.com>
2022-12-07 17:06:06 +0900

    fix: zoom 컴포넌트 분리, 확대축소시 렌더링되던것 해결

--- client/src/MainPage/MainWorld.tsx
+++ client/src/MainPage/MainWorld.tsx
@@ -1,41 +1,25 @@
-import { Suspense, useEffect, useState } from "react";
-import { useFrame, useThree } from "@react-three/fiber";
-import { Physics } from "@react-three/rapier";
+import { Suspense } from "react";
+import { Debug, Physics } from "@react-three/rapier";
 import Light from "../GalleryPage/mapObjects/Light";
 
 import Player from "./mapObjects/Player";
 import Plane from "./mapObjects/Plane";
 import MapDataFetcher from "./components/MapDataFetcher";
+import Ground from "./mapObjects/Ground";
+import Zoom from "./components/Zoom";
 
 export default function MainWorld() {
-  const { camera } = useThree();
-  const [cameraScale, setCameraScale] = useState(1);
-
-  useFrame(() => {
-    camera.scale.set(cameraScale, cameraScale, cameraScale);
-  });
-
-  useEffect(() => {
-    function wheelHandler(e: WheelEvent) {
-      if (cameraScale < 1.5 && e.deltaY > 0) {
-        setCameraScale((prev) => prev + e.deltaY / 1000);
-      }
-      if (cameraScale > 0.2 && e.deltaY < 0) {
-        setCameraScale((prev) => prev + e.deltaY / 1000);
-      }
-    }
-    document.addEventListener("wheel", wheelHandler);
-    return () => document.removeEventListener("wheel", wheelHandler);
-  }, [cameraScale]);
-
   return (
     <Physics gravity={[0, 0, 0]}>
+      <Debug />
       <Suspense fallback={null}>
         <MapDataFetcher />
       </Suspense>
       <Light />
+      <Ground />
       <Player />
       <Plane />
+      <Zoom />
     </Physics>
   );
 }

a546b630b8262faad90178965904d449128494aa
Seyeon Ko <koseyeon97@gmail.com>
2022-12-08 18:58:37 +0900

    feat: ground 가 플레이어의 이동에 따라 움직이고, texture도 자연스럽게 보이도록 구현

--- client/src/MainPage/mapObjects/Ground.tsx
+++ client/src/MainPage/mapObjects/Ground.tsx
@@ -3,15 +3,25 @@ import { useTexture } from "@react-three/drei";
 import galleryStore from "../../store/gallery.store";
 import { THEME } from "../../@types/gallery";
 import { GROUND_TEXTURES } from "../../@types/main";
+import { useFrame, useThree } from "@react-three/fiber";
+import { useRef } from "react";
 
 export default function Ground() {
   const theme = galleryStore((store) => store.theme);
   const texture = useTexture(GROUND_TEXTURES[theme || THEME.DREAM]);
   texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+  const { camera } = useThree();
+  const groundRef = useRef<THREE.Mesh>(null!);
+  useFrame(() => {
+    texture.offset.x = camera.position.x / 10;
+    texture.offset.y = -camera.position.z / 10;
+    groundRef.current.position.x = camera.position.x;
+    groundRef.current.position.z = camera.position.z - 20;
+  });
   return (
-    <mesh receiveShadow position={[0, 0.1, 0]} rotation-x={-Math.PI / 2}>
-      <planeGeometry args={[1000, 1000]} />
-      <meshStandardMaterial map={texture} map-repeat={[100, 100]} color="#999999" />
+    <mesh receiveShadow ref={groundRef} position={[0, 0.1, 0]} rotation-x={-Math.PI / 2}>
+      <planeGeometry args={[50, 50]} />
+      <meshStandardMaterial map={texture} map-repeat={[5, 5]} color="#999999" />
     </mesh>
   );
 }

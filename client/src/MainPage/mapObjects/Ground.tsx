e458344f4141b32d6a7222f614e0639c9ae81c0b
Seyeon Ko <koseyeon97@gmail.com>
2022-12-08 15:50:41 +0900

    chore:theme 별 ground 텍스쳐 추가

--- client/src/MainPage/mapObjects/Ground.tsx
+++ client/src/MainPage/mapObjects/Ground.tsx
@@ -1,14 +1,18 @@
 import * as THREE from "three";
 import { useTexture } from "@react-three/drei";
-import grass from "../../assets/images/grass.png";
+import springGround from "../../assets/images/spring-ground.png";
+import summerGround from "../../assets/images/summer-ground.png";
+import autumnGround from "../../assets/images/autumn-ground.png";
+import winterGround from "../../assets/images/winter-ground.png";
+import dreamGround from "../../assets/images/dream-ground.png";
 
 export default function Ground() {
-  const texture = useTexture(grass);
+  const texture = useTexture(springGround);
   texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
   return (
     <mesh receiveShadow position={[0, 0.1, 0]} rotation-x={-Math.PI / 2}>
       <planeGeometry args={[1000, 1000]} />
-      <meshStandardMaterial map={texture} map-repeat={[100, 100]} color="#5a5a5a" />
+      <meshStandardMaterial map={texture} map-repeat={[100, 100]} color="#999999" />
     </mesh>
   );
 }

13105ac1b8be9bb822d9bb0668e88f6aebb261c6
kddkdkk <dlwjddnrbb71@naver.com>
2022-12-13 00:07:59 +0900

    fix: MainWorld -> MainPage로 useError 위치 변경
    왜 MainWorld에 넣어뒀지? 알수가없는..

--- client/src/MainPage/MainWorld.tsx
+++ client/src/MainPage/MainWorld.tsx
@@ -8,20 +8,8 @@ import MapDataFetcher from "./components/MapDataFetcher";
 import Ground from "./mapObjects/Ground";
 import Environments from "./mapObjects/Environments";
 import DevTools from "../components/Devtools";
-import useError from "../hooks/useError";
-import toastStore from "../store/toast.store";
-import TOAST from "../components/Toast/ToastList";
-import mainStore from "../store/main.store";
 
 export default function MainWorld() {
-  const addToast = toastStore((store) => store.addToast);
-  const setShowSplash = mainStore((store) => store.setShowSplash);
-
-  useError((reason) => {
-    addToast(TOAST.ERROR(reason));
-    setShowSplash(false);
-  });
-
   return (
     <Physics gravity={[0, 0, 0]}>
       <ErrorBoundary fallback={null}>

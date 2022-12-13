6c685d9745c332406fbfb5d415998c0653b156d3
lybell <futireflugel@gmail.com>
2022-12-13 20:51:35 +0900

    refactor: 부제목 돌 리팩토링

--- client/src/GalleryPage/mapObjects/MemorialStone.tsx
+++ client/src/GalleryPage/mapObjects/MemorialStone.tsx
@@ -1,5 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from "react";
-import { Object3D } from "three";
+import { useEffect, useMemo, useState } from "react";
 import { Text } from "@react-three/drei";
 import { animated } from "@react-spring/three";
 
@@ -7,9 +6,15 @@ import Pedestal from "./Pedestal";
 import { useBillboard } from "../../hooks/useBillboard";
 
 import { COLORS } from "../../@types/colors";
+import type { Mesh } from "three";
 import type { ITriggeredSpringState } from "../../@types/animator";
 import type { IGalleryPageSubTitle } from "../../@types/gallery";
 import MapoFont from "../../assets/fonts/MapoFlowerIsland.otf";
+
+interface ElevateTextProps {
+  text: string;
+  textSize: number;
+}
 
 interface MemorialStonesProps {
   subtitles: IGalleryPageSubTitle[];
@@ -27,7 +32,14 @@ interface IStoneInfo {
   stonePosition: number[];
 }
 
-function calculateMemorialStonePosition(subtitles: IGalleryPageSubTitle[]) {
+const MAX_VISIBLE_LENGTH = 10;
+
+function insertSpace(str: string) {
+  const result = str.replace(/\s/g, "·").replace(/(.)/g, "$1 ");
+  return result;
+}
+
+function calculateMemorialStonePosition(subtitles: IGalleryPageSubTitle[]): IStoneInfo[] {
   const enalblePositions = [
     [0, 0],
     [1, 1],
@@ -37,144 +49,77 @@ function calculateMemorialStonePosition(subtitles: IGalleryPageSubTitle[]) {
     [3, 1],
     [-3, -1],
   ];
-  let cursor = 0;
-  const stoneInfoList: IStoneInfo[] = [];
-  const h1List = subtitles.filter((subTitle: IGalleryPageSubTitle) => subTitle.hType === "h1");
-  const h2List = subtitles.filter((subTitle: IGalleryPageSubTitle) => subTitle.hType === "h2");
-  const h3List = subtitles.filter((subTitle: IGalleryPageSubTitle) => subTitle.hType === "h3");
-  while (cursor < enalblePositions.length) {
-    if (h1List.length > 0) {
-      const subtitle = h1List.pop();
-      if (subtitle) {
-        stoneInfoList.push({
-          subtitle,
-          stonePosition: enalblePositions[cursor],
-        });
-        cursor++;
-        continue;
-      }
-    } else if (h2List.length > 0) {
-      const subtitle = h2List.pop();
-      if (subtitle) {
-        stoneInfoList.push({
-          subtitle,
-          stonePosition: enalblePositions[cursor],
-        });
-        cursor++;
-        continue;
-      }
-      cursor++;
-      continue;
-    } else if (h3List.length > 0) {
-      const subtitle = h3List.pop();
-      if (subtitle) {
-        stoneInfoList.push({
-          subtitle,
-          stonePosition: enalblePositions[cursor],
-        });
-        cursor++;
-        continue;
-      }
-    } else {
-      break;
+  function getHtypePriority(hType: string): number {
+    if (typeof hType === "string" && hType[0] === "h") {
+      return Number(hType[1]);
     }
+    return 4;
   }
-  return stoneInfoList;
+
+  return [...subtitles]
+    .sort((a, b) => getHtypePriority(a.hType) - getHtypePriority(b.hType))
+    .slice(0, enalblePositions.length)
+    .map((subtitle: IGalleryPageSubTitle, i: number) => ({
+      subtitle,
+      stonePosition: enalblePositions[i],
+    }));
 }
 
-function textPreProcessing(text: string) {
-  const letters = text.split("");
-  let visibleLetters: string[] = [];
-  let invisibleLetters: string[] = [];
-  if (letters.length > 10) {
-    visibleLetters = letters.slice(0, 10);
-    invisibleLetters = letters.slice(10);
-  } else {
-    visibleLetters = letters;
-  }
-  return { visibleLetters, invisibleLetters };
-}
 function getStyleByTitleType(type: string) {
-  let textSize = 0;
-  let stoneColor = "";
   switch (type) {
     case "h1":
-      textSize = 0.6;
-      stoneColor = COLORS.BROWN200;
-      break;
+      return { textSize: 0.6, stoneColor: COLORS.BROWN200 };
     case "h2":
-      textSize = 0.4;
-      stoneColor = COLORS.BROWN100;
-      break;
+      return { textSize: 0.4, stoneColor: COLORS.BROWN100 };
     case "h3":
-      textSize = 0.3;
-      stoneColor = COLORS.BROWN50;
-      break;
+      return { textSize: 0.3, stoneColor: COLORS.BROWN50 };
     default:
-      break;
+      return { textSize: 0, stoneColor: "#222222" };
   }
-  return { textSize, stoneColor };
+}
+
+function ElevateText({ text, textSize }: ElevateTextProps) {
+  const [textIndex, setTextIndex] = useState(0);
+  const visibleText = useMemo(() => {
+    if (text.length <= MAX_VISIBLE_LENGTH) return insertSpace(text);
+    return insertSpace((text + text).slice(textIndex, textIndex + MAX_VISIBLE_LENGTH));
+  }, [text, textIndex]);
+
+  useEffect(() => {
+    if (text.length <= MAX_VISIBLE_LENGTH) return;
+
+    const animation = setInterval(() => {
+      setTextIndex((idx) => (idx + 1) % text.length);
+    }, 300);
+    return () => {
+      clearInterval(animation);
+    };
+  }, [text]);
+
+  return (
+    <Text font={MapoFont} fontSize={textSize} color={"black"} maxWidth={0.1} textAlign={"center"} lineHeight={1}>
+      {visibleText}
+    </Text>
+  );
 }
 
 function MemorialStone({ subTitle, position, animator }: MemorialStoneProps) {
   const { text, hType } = subTitle;
   const { textSize, stoneColor } = getStyleByTitleType(hType);
-  const [pText, setPText] = useState("");
 
-  const { visibleLetters, invisibleLetters } = useMemo(() => {
-    const { visibleLetters, invisibleLetters } = textPreProcessing(text);
-    setPText(visibleLetters.join(" "));
-    return { visibleLetters, invisibleLetters };
-  }, []);
-  const yPositionOrigin = textSize * (visibleLetters.length / 2) + 0.5;
+  const subtitleMeshRef = useBillboard<Mesh>({ lockElevation: true });
 
-  const subtitleMeshRef = useBillboard<THREE.Mesh>({ lockElevation: true });
-  const subtitleRef = useRef<Object3D>(null);
-
+  const yPositionOrigin = textSize * (Math.min(text.length, MAX_VISIBLE_LENGTH) / 2) + 0.5;
   const { spring, active } = animator;
   const yPosition = useMemo(() => spring.to([0, 1], [-yPositionOrigin, yPositionOrigin]), []);
   const scale = useMemo(() => spring.to([0, 1], [0, 1]), []);
-
-  useEffect(() => {
-    const needAnimation = invisibleLetters.length > 0 ? true : false;
-    let animation: ReturnType<typeof setInterval>;
-    if (needAnimation) {
-      animation = setInterval(() => {
-        if (!subtitleRef.current) return;
-        const letter = visibleLetters.pop();
-        if (letter) {
-          invisibleLetters.push(letter);
-        }
-        const newLetter = invisibleLetters.shift();
-        if (newLetter) {
-          visibleLetters.unshift(newLetter);
-        }
-        setPText(visibleLetters.join(" "));
-      }, 300);
-    }
-    return () => {
-      if (needAnimation) {
-        clearInterval(animation);
-      }
-    };
-  }, []);
 
   return (
     <group position={[position[0], 0, position[1]]} scale={textSize}>
       <Pedestal scale={0.5} color={stoneColor} />
       <animated.group position-y={yPosition} scale={scale} visible={active}>
         <mesh ref={subtitleMeshRef}>
-          <Text
-            ref={subtitleRef}
-            font={MapoFont}
-            fontSize={textSize}
-            color={"black"}
-            maxWidth={0.1}
-            textAlign={"center"}
-            lineHeight={1}
-          >
-            {pText}
-          </Text>
+          <ElevateText text={text} textSize={textSize} />
         </mesh>
       </animated.group>
     </group>
